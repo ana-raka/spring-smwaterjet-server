bb36c4e098e8bb865b47d1212b105e55c647d856
UiChan Lee <junglkls1234@gmail.com>
2025-01-22 15:24:55 +0900

    project init
    프로젝트의 .ignore 등록 및 초기 설정

---
+++ src/test/java/com/smwaterjet/smwaterjet/SmwaterjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smwaterjet.smwaterjet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmwaterjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
